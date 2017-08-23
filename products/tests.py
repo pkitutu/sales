a9d2dd6257a2a92f63fb35deff8649fb5d97c0f6
Kitutu <pkitutu@gmail.com>
2017-08-23 11:45:23 +0300

    test case for product sales

--- products/tests.py
+++ products/tests.py
@@ -3,15 +3,15 @@ from .models import Products
 
 # Create your tests here.
 
-class ProductsalesTestCase(TestCase):
+class ProductTestCase(TestCase):
 	def setUp(self):
-		Products.objects.create(product_name='Shoesk', buying_price=10, selling_price=20)
-		Products.objects.create(product_name='Slippersk', buying_price=15, selling_price=17)
+		Products.objects.create(product_name='Shoes', buying_price=10, selling_price=20)
+		Products.objects.create(product_name='Slippers', buying_price=15, selling_price=17)
 		
 
 	def test_products(self):
-		product1 = Products.objects.get(product_name='Shoesk')
-		product2 = Products.objects.get(product_name='Slippersk')
+		product1 = Products.objects.get(product_name='Shoes')
+		product2 = Products.objects.get(product_name='Slippers')
 		self.assertEqual(product1.buying_price, 10)
 		self.assertEqual(product1.selling_price, 20)
 		self.assertEqual(product2.buying_price, 15)
--- productsales/tests.py
+++ productsales/tests.py
@@ -1,1 +1,21 @@
 from django.test import TestCase
+from .models import ProductSales
+from products.models import Products
+
+# Create your tests here.
+
+class ProductsalesTestCase(TestCase):
+	def setUp(self):
+		product1 = Products.objects.create(product_name='Shoes', buying_price=10, selling_price=20)
+		product2 = Products.objects.create(product_name='Slippers', buying_price=15, selling_price=17)
+		ProductSales.objects.create(pk=1,product=product1, units_sold=5, selling_date='2017-01-03')
+		ProductSales.objects.create(pk=2,product=product2, units_sold=6, selling_date='2017-01-03')
+		ProductSales.objects.create(pk=3,product=product2, units_sold=10, selling_date='2017-01-03')
+
+	def test_product_sales(self):
+		sale1 = ProductSales.objects.get(pk=1)
+		sale2 = ProductSales.objects.get(pk=2)
+		sale3 = ProductSales.objects.get(pk=3)
+		self.assertEqual(sale1.units_sold, 5)
+		self.assertEqual(sale2.units_sold, 6)
+		self.assertEqual(sale3.units_sold, 10)
